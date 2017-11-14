8cfe94f0efaa657c1d68b1648cc951352220b0c2
A A <anthony.arroyo@cix.csi.cuny.edu>
2017-11-14 01:14:03 -0500

    Checkers updated, defunct currently.

--- Project1/Checkers.cpp
+++ Project1/Checkers.cpp
@@ -10,9 +10,10 @@ const int row = 8;
 const int element = 4;
 void zPrintBoard(int *display[row][row], string(&part)[11]);
 void zPrintBoard(int *display[row][row], string(&part)[11], string player);
+int inputScrambler();
 
 int Checkers() {
-	bool diagnostic = 0; bool yes = 0; bool normal = 1;
+	bool diagnostic = 0; bool yes = 0; bool normal = 1; bool playing = 1;
 	string parts[11] = { " ", " X", " O", " x", " o", ">X", ">O", ">x", ">o", "---", " |" };
 
 	int null = 0;
@@ -71,7 +72,12 @@ int Checkers() {
 		}
 	}*/
 
-	zPrintBoard(displayboard, parts);
+
+	while (playing) {
+		zPrintBoard(displayboard, parts);
+		inputScrambler();
+
+	}
 
 
 	cin >> pause;
@@ -136,4 +142,53 @@ void zPrintBoard(int *display[row][row], string (&part)[11], string player) {
 		cout << endl;
 	}
 	cout << "\n  Please enter your next move, Player " << player << ".  ";
+}
+
+bool ErrorCheck(char in[6]) {
+	int i = 0;
+
+	do {
+		for (int j = 0; j < 2; j++) {
+			// If character is a number 1-8, check if next character is a letter A-H
+			if (in[i] > 48 && in[i] < 57)
+				if (toupper(in[i++]) > 64 && toupper(in[i++]) < 73) {
+					cout << '\n' << in[i] << ' ' << i << endl;
+				}
+				else {
+					cout << '\n' << in[i] << '*' << in[i++] << '=' << i << endl;
+					return 1;
+				}
+			else if (toupper(in[i]) > 64 && toupper(in[i]) < 73)
+				if (in[i++] > 48 && in[i++] < 57);
+				else return 1;
+
+				cout << i << ' ' << j << endl;
+		}
+		// Skips the middle character
+		cout << i;
+		i++;
+		cout << i;
+	} while (i < 5);
+
+	cout << i;
+	return 0;
+}
+
+int inputScrambler() {
+	char input[6];
+	bool done = 0;
+
+	while (!done) {
+		// Ask for input
+		cin.getline(input, 6);
+
+		// If no error, stop asking
+		if (!ErrorCheck(input)) {
+			done = 1;
+			cout << "Good.";
+		}
+		else cout << "Error.";
+	}
+
+	return 0;
 }
